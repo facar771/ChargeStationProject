078f530a25aed0fbceb8f2117965dc6020bfd0bc
facar771 <59576219+facar771@users.noreply.github.com>
2024-01-05 10:13:21 +0300

    ChargeStation

--- Mobil/components/IP-5.js
+++ Mobil/components/IP-5.js
@@ -5,7 +5,7 @@ const WarningScreen = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
-        Kişi bilgileri değerli olduğundan bu sayfa IP-5 ile birlikte tamamlanacaktir.
+        Kişi bilgileri değerli oldugundan bu sayfa IP-5 ile birlikte tamamlanacaktir.
       </Text>
     </View>
   );
